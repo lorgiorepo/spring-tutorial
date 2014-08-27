789d87ba6b4ad24b1e2c5d80ed3443764395d879
Lorgio Trinidad <lorgio@saturno.(none)>
2014-08-27 02:09:59 -0400

    Proyecto MVC con Spring Framework 3, prueba controlador home

---
+++ spring-mvc-01/src/main/java/com/lorgio/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.lorgio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String gotToHome(ModelMap model){
+		model.addAttribute("message", "Hola Mundo");
+		return "home";
+	}
+}
